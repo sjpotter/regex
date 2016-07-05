7c47693dabd87ef552d9573a9284ee41fde1fb02
Shaya Potter <spotter@gmail.com>
2016-07-05 14:31:55 -0700

    Greedy matching is broken in complicated cases.

--- src/test/java/org/yucs/spotter/regex/LookBehindTests.java
+++ src/test/java/org/yucs/spotter/regex/LookBehindTests.java
@@ -18,4 +18,19 @@ public class LookBehindTests {
         assertTrue(r.match("cab"));
         assertFalse(r.match("bed"));
     }
+
+    @Test
+    public void threeLetter() throws Exception {
+        Regex r = new Regex("(?<=cab)b");
+        assertTrue(r.match("cabb"));
+        assertFalse(r.match("bedb"));
+    }
+
+    // Implementation doesn't backtrack greedy correctly.
+    @Test
+    public void broken() throws Exception {
+        Regex r = new Regex("c(.*)(?<=\\1)b");
+        Matcher m = r.Matcher();
+        assertTrue(m.match("cab"));
+    }
 }
