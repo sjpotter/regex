52209df55ce3e727cf8e273b4025cc85d75d4c52
Shaya Potter <spotter@gmail.com>
2016-07-08 00:49:04 -0700

    make capturing work right in the face of backtracking failures

--- src/main/java/org/yucs/spotter/regex/EndCaptureToken.java
+++ src/main/java/org/yucs/spotter/regex/EndCaptureToken.java
@@ -4,6 +4,8 @@ class EndCaptureToken extends Token {
     final private StartCaptureToken start;
     final private int capture_pos;
 
+    int invalid_capture = 1;
+
     EndCaptureToken(Token t, int pos) {
         start = (StartCaptureToken) t;
         capture_pos = pos;
@@ -12,11 +14,13 @@ class EndCaptureToken extends Token {
     @Override
     boolean match(Matcher m) throws RegexException {
         m.pushGroup(capture_pos, m.getText().substring(start.start_pos, m.getTextPosition()));
+
         if (next.match(m))
             return true;
 
-        // i.e. this isn't a valid path, so this isn't a valid capture.
         m.popGroup(capture_pos);
+
+        // i.e. this isn't a valid path, so this isn't a valid capture.
         return false;
     }
 }
--- src/main/java/org/yucs/spotter/regex/StartCaptureToken.java
+++ src/main/java/org/yucs/spotter/regex/StartCaptureToken.java
@@ -1,11 +1,20 @@
 package org.yucs.spotter.regex;
 
+import java.util.Stack;
+
 class StartCaptureToken extends Token {
     int start_pos;
 
     @Override
     boolean match(Matcher m) throws RegexException {
+        int old_start_pos = start_pos;
+
         start_pos = m.getTextPosition();
-        return next.match(m);
+        boolean ret = next.match(m);
+
+        if (!ret)
+            start_pos = old_start_pos;
+
+        return ret;
     }
 }
